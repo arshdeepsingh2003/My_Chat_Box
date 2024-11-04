e2294cb72f13427a9e5880de3b7e9522042949f1
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2024-11-04 18:08:38 +0530

    created the model of user and get all users route

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,18 @@
-export const getAllUsers=()=>{
-    //get all the users
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+
+export const getAllUsers=async(
+    req:Request,
+    res:Response,
+    next:NextFunction
+)=>{
+    try {
+        //gt all users
+        const users=await User.find();
+        return res.status(200).json({message:"OK",users});
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({message:"ERROR",cause:error.message})
+        
+    }
 }
---
+++ backend/src/models/User.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from "crypto";
+import mongoose from "mongoose";
+const chatsSchema=new mongoose.Schema({
+    id:{
+        type:String,
+        default:randomUUID(),
+                    
+    },
+    role:{
+        type:String,
+        required:true,
+    },
+    content:{
+        type:String,
+        required:true,
+    },
+});
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    chats:[chatsSchema],
+});
+
+
+export default mongoose.model("User",userSchema);
