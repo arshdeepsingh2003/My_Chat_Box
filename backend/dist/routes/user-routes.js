d5a3163f7e6fd2ab0125fc7d64e05aa1ab218136
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2024-11-04 21:05:46 +0530

    completed sign up api

--- backend/dist/routes/user-routes.js
+++ backend/dist/routes/user-routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
-import { getAllUsers } from '../controllers/user-controllers.js';
+import { getAllUsers, userSignup } from '../controllers/user-controllers.js';
+import { validate, signupValidator } from '../utils/validators.js';
 const userRoutes = Router();
 userRoutes.get("/", getAllUsers);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
 export default userRoutes;
 //# sourceMappingURL=user-routes.js.map
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,7 +1,9 @@
-import {Router} from 'express';
-import { getAllUsers } from '../controllers/user-controllers.js';
+import { Router } from 'express';
+import { getAllUsers, userSignup } from '../controllers/user-controllers.js';
+import { validate, signupValidator } from '../utils/validators.js';
 
-const userRoutes=Router();
-userRoutes.get("/",getAllUsers)
+const userRoutes = Router();
+userRoutes.get("/", getAllUsers);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
 
 export default userRoutes;
