acf46312e65b420db4f6b6ec9668f667fc9e0197
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2024-11-05 12:29:43 +0530

    created tokens and added authorisation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,15 @@ import express from "express";
 import {config} from "dotenv";
 import morgan from 'morgan';
 import appRouter from "./routes/index.js";
+import { cookie } from "express-validator";
+import cookieParser from "cookie-parser";
 
 config();
 const app=express();
 
 //middlewares
 app.use(express.json()); 
-
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //remove t in the production
 app.use(morgan("dev"));
