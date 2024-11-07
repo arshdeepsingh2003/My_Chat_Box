c785b2c316339dc6f41a2ef00d3d369b69d00923
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2024-11-07 12:31:32 +0530

    added header and pictures

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+
+import AppBar  from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Logo from './shared/Logo';
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <AppBar sx={{bgcolor:"transparent",position:"static",boxShadow:"none"}}>
+        <Toolbar sx={{display:"flex"}}><Logo></Logo></Toolbar>
+    </AppBar>
+  );
+};
